4267169245aafd8891cae5167f7bab313b71ec9e
Ayah Dahmani <ayadah@alumn.us.es>
2024-04-11 21:58:01 +0200

    adding something en la api

--- trying_api/google_trend_api.py
+++ trying_api/google_trend_api.py
@@ -1,9 +1,13 @@
-# el primer end
+
+from http.client import HTTPException
+import os
 from pytrends.request import TrendReq
 from fastapi import FastAPI
 import pandas as pd
 import time
 import requests
+
+from fastapi.responses import RedirectResponse
 
 
 
@@ -11,16 +15,17 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to my Gtrend_API application!"}
+    return RedirectResponse(url="/docs")
 
 url = 'https://trends.google.com/trends/'
 response = requests.get(url, timeout=10)
 
 
-country_list = ['AT','BE', 'CH', 'DE', 'DK', 'EE', 'GR', 'ES',
+country_list = ['BE', 'CH', 'DE', 'DK', 'EE', 'GR', 'ES',
        'FI', 'FR', 'HU', 'IE', 'IS', 'IT','LU', 'ME',
        'MT', 'NL', 'NO', 'PL', 'PT', 'SE', 'SI','GB',
         'LT', 'LV']
+
 
 def obtener_datos_google_trends_mensual(palabra_clave, paises=country_list, fecha_inicio='2014-01-01', fecha_fin='2024-03-31'):
     try:
@@ -49,9 +54,43 @@ def google_trends_handler(topic: str, fecha_inicio: str = '2014-01-01', fecha_fi
     trends_data = obtener_datos_google_trends_mensual(topic, country_list, fecha_inicio, fecha_fin)
     return trends_data
 
+
+@app.get("/save-csv/{topic}")
+def save_csv_handler(topic: str):
+    try:
+    
+        # Obtain data
+        datos = obtener_datos_google_trends_mensual(topic)
+
+        # Convert data to DataFrame
+        df = pd.DataFrame.from_dict({(i, j): datos[i][j] 
+                                      for i in datos.keys() 
+                                      for j in datos[i].keys()},
+                                     orient='index')
+
+        # Create directory if it doesn't exist
+        directory = 'data'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        # Save DataFrame as CSV
+        df.to_csv(os.path.join(directory, f'datos_{topic}.csv'), index=True, encoding='utf-8')
+        
+        return {"message": f"CSV file for '{topic}' saved successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+# para la limpieza we will use the same endpoints of gdelt that is  what we agreed on with the tea
+
+
+
+
+
+
+
+
 #ejemplo del uso 
 
-#palabra_clave = "Unemployment"
+palabra_clave = "Unemployment"
 #datos8 = obtener_datos_google_trends_mensual(palabra_clave)
 #atos8
 
